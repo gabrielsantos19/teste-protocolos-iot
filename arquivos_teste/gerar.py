ac03155d6c54b115c21b24eb14491ac16dabb3e7
Gabriel Santos <25865943+gabrielsantos19@users.noreply.github.com>
2022-08-07 17:02:37 -0300

    Add files via upload

--- arquivos_teste/gerar.py
+++ arquivos_teste/gerar.py
@@ -5,7 +5,7 @@ def get_linha():
     limite_inferior = ord('a')
     limite_superior = ord('z')
     r = range(limite_inferior, limite_superior)
-    c = random.choices(r, k=200)
+    c = random.choices(r, k=125)
     linha = ''.join([chr(x) for x in c])
     return linha
 
@@ -14,13 +14,14 @@ def gerar_arquivo(nome, numero_de_linhas):
     with open(nome, 'w') as f:
         for i in range(numero_de_linhas):
             l = get_linha()
-            print(l, file=f)
+            print(f"{i:04}_{l}", file=f)
 
 
 def main():
-    gerar_arquivo('arquivo_teste_1.txt', 300)
-    gerar_arquivo('arquivo_teste_2.txt', 600)
-    gerar_arquivo('arquivo_teste_3.txt', 1000)
+    gerar_arquivo('cenario_1.txt', 30)
+    gerar_arquivo('cenario_2.txt', 30)
+    gerar_arquivo('cenario_3.txt', 900)
+    gerar_arquivo('cenario_4.txt', 900)
 
 
 if __name__ == '__main__':
